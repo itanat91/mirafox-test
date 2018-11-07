7b3ea9b0469a03950a3e72582449f6857f399c02
Танат <t.aitkaliev@newcontact.su>
2018-11-07 22:25:55 +0400

    init commit

---
+++ data.php
@@ -0,0 +1,356 @@
+<?php
+
+$data = [
+	0 => [
+		'name' => 'Бразилия', # название команды
+		'games' => 104, # всего игр
+		'win' => 70, # количество побед
+		'draw' => 17, # количестве ничей
+		'defeat' => 17, # количество поражений
+		'goals' => [
+			'scored' => 221, # всего забито голов
+			'skipped' => 102, # всего пропущено голов
+		],
+	],
+	1 => [
+		'name' => 'Германия / ФРГ',
+		'games' => 106,
+		'win' => 66,
+		'draw' => 20,
+		'defeat' => 20,
+		'goals' => [
+			'scored' => 224,
+			'skipped' => 121,
+		],
+	],
+	2 => [
+		'name' => 'Италия',
+		'games' => 83,
+		'win' => 45,
+		'draw' => 21,
+		'defeat' => 17,
+		'goals' => [
+			'scored' => 128,
+			'skipped' => 77,
+		],
+	],
+	3 => [
+		'name' => 'Аргентина',
+		'games' => 77,
+		'win' => 42,
+		'draw' => 14,
+		'defeat' => 21,
+		'goals' => [
+			'scored' => 131,
+			'skipped' => 84,
+		],
+	],
+	4 => [
+		'name' => 'Англия',
+		'games' => 62,
+		'win' => 26,
+		'draw' => 20,
+		'defeat' => 16,
+		'goals' => [
+			'scored' => 79,
+			'skipped' => 56,
+		],
+	],
+	5 => [
+		'name' => 'Испания',
+		'games' => 59,
+		'win' => 29,
+		'draw' => 12,
+		'defeat' => 18,
+		'goals' => [
+			'scored' => 92,
+			'skipped' => 66,
+		],
+	],
+	6 => [
+		'name' => 'Франция',
+		'games' => 59,
+		'win' => 28,
+		'draw' => 12,
+		'defeat' => 19,
+		'goals' => [
+			'scored' => 106,
+			'skipped' => 71,
+		],
+	],
+	7 => [
+		'name' => 'Голландия',
+		'games' => 50,
+		'win' => 27,
+		'draw' => 12,
+		'defeat' => 11,
+		'goals' => [
+			'scored' => 86,
+			'skipped' => 48,
+		],
+	],
+	8 => [
+		'name' => 'Уругвай',
+		'games' => 51,
+		'win' => 20,
+		'draw' => 12,
+		'defeat' => 19,
+		'goals' => [
+			'scored' => 80,
+			'skipped' => 71,
+		],
+	],
+	9 => [
+		'name' => 'Швеция',
+		'games' => 46,
+		'win' => 16,
+		'draw' => 13,
+		'defeat' => 17,
+		'goals' => [
+			'scored' => 74,
+			'skipped' => 69,
+		],
+	],
+	10 => [
+		'name' => 'Россия / СССР',
+		'games' => 40,
+		'win' => 17,
+		'draw' => 8,
+		'defeat' => 15,
+		'goals' => [
+			'scored' => 66,
+			'skipped' => 47,
+		],
+	],
+	11 => [
+		'name' => 'Сербия / Югославия    ',
+		'games' => 43,
+		'win' => 17,
+		'draw' => 8,
+		'defeat' => 18,
+		'goals' => [
+			'scored' => 64,
+			'skipped' => 59,
+		],
+	],
+	12 => [
+		'name' => 'Мексика',
+		'games' => 53,
+		'win' => 14,
+		'draw' => 14,
+		'defeat' => 25,
+		'goals' => [
+			'scored' => 57,
+			'skipped' => 92,
+		],
+	],
+	13 => [
+		'name' => 'Бельгия',
+		'games' => 41,
+		'win' => 14,
+		'draw' => 9,
+		'defeat' => 18,
+		'goals' => [
+			'scored' => 52,
+			'skipped' => 66,
+		],
+	],
+	14 => [
+		'name' => 'Польша',
+		'games' => 31,
+		'win' => 15,
+		'draw' => 5,
+		'defeat' => 11,
+		'goals' => [
+			'scored' => 44,
+			'skipped' => 40,
+		],
+	],
+	15 => [
+		'name' => 'Венгрия',
+		'games' => 32,
+		'win' => 15,
+		'draw' => 3,
+		'defeat' => 14,
+		'goals' => [
+			'scored' => 87,
+			'skipped' => 57,
+		],
+	],
+	16 => [
+		'name' => 'Португалия',
+		'games' => 26,
+		'win' => 13,
+		'draw' => 4,
+		'defeat' => 9,
+		'goals' => [
+			'scored' => 43,
+			'skipped' => 29,
+		],
+	],
+	17 => [
+		'name' => 'Чехия / Чехословакия',
+		'games' => 33,
+		'win' => 12,
+		'draw' => 5,
+		'defeat' => 16,
+		'goals' => [
+			'scored' => 47,
+			'skipped' => 49,
+		],
+	],
+	18 => [
+		'name' => 'Чили',
+		'games' => 33,
+		'win' => 11,
+		'draw' => 7,
+		'defeat' => 15,
+		'goals' => [
+			'scored' => 40,
+			'skipped' => 49,
+		],
+	],
+	19 => [
+		'name' => 'Австрия',
+		'games' => 29,
+		'win' => 12,
+		'draw' => 4,
+		'defeat' => 13,
+		'goals' => [
+			'scored' => 43,
+			'skipped' => 47,
+		],
+	],
+	20 => [
+		'name' => 'Швейцария',
+		'games' => 33,
+		'win' => 11,
+		'draw' => 6,
+		'defeat' => 16,
+		'goals' => [
+			'scored' => 45,
+			'skipped' => 59,
+		],
+	],
+	21 => [
+		'name' => 'Парагвай',
+		'games' => 27,
+		'win' => 7,
+		'draw' => 10,
+		'defeat' => 10,
+		'goals' => [
+			'scored' => 30,
+			'skipped' => 38,
+		],
+	],
+	22 => [
+		'name' => 'США',
+		'games' => 33,
+		'win' => 8,
+		'draw' => 6,
+		'defeat' => 19,
+		'goals' => [
+			'scored' => 37,
+			'skipped' => 62,
+		],
+	],
+	23 => [
+		'name' => 'Румыния',
+		'games' => 21,
+		'win' => 8,
+		'draw' => 5,
+		'defeat' => 8,
+		'goals' => [
+			'scored' => 30,
+			'skipped' => 32,
+		],
+	],
+	24 => [
+		'name' => 'Южная Корея',
+		'games' => 31,
+		'win' => 5,
+		'draw' => 9,
+		'defeat' => 17,
+		'goals' => [
+			'scored' => 31,
+			'skipped' => 67,
+		],
+	],
+	25 => [
+		'name' => 'Дания',
+		'games' => 16,
+		'win' => 8,
+		'draw' => 2,
+		'defeat' => 6,
+		'goals' => [
+			'scored' => 27,
+			'skipped' => 24,
+		],
+	],
+	26 => [
+		'name' => 'Хорватия',
+		'games' => 16,
+		'win' => 7,
+		'draw' => 2,
+		'defeat' => 7,
+		'goals' => [
+			'scored' => 21,
+			'skipped' => 17,
+		],
+	],
+	27 => [
+		'name' => 'Колумбия',
+		'games' => 18,
+		'win' => 7,
+		'draw' => 2,
+		'defeat' => 9,
+		'goals' => [
+			'scored' => 26,
+			'skipped' => 27,
+		],
+	],
+	28 => [
+		'name' => 'Шотландия',
+		'games' => 23,
+		'win' => 4,
+		'draw' => 7,
+		'defeat' => 12,
+		'goals' => [
+			'scored' => 25,
+			'skipped' => 41,
+		],
+	],
+	29 => [
+		'name' => 'Камерун',
+		'games' => 23,
+		'win' => 4,
+		'draw' => 7,
+		'defeat' => 12,
+		'goals' => [
+			'scored' => 18,
+			'skipped' => 43,
+		],
+	],
+	30 => [
+		'name' => 'Коста-Рика',
+		'games' => 15,
+		'win' => 5,
+		'draw' => 4,
+		'defeat' => 6,
+		'goals' => [
+			'scored' => 17,
+			'skipped' => 23,
+		],
+	],
+	31 => [
+		'name' => 'Болгария',
+		'games' => 26,
+		'win' => 3,
+		'draw' => 8,
+		'defeat' => 15,
+		'goals' => [
+			'scored' => 22,
+			'skipped' => 53,
+		],
+	],
+];
---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Танат
+ * Date: 07.11.2018
+ * Time: 22:05
+ */
+
